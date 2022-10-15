5c82253459b62d2f9c7411460b15b1d3309a52f4
cmd <cmd@yggdrasil.onion>
2022-10-15 14:34:18 -0500

    update

---
+++ examples/ln-gateway/node/app.js
@@ -0,0 +1,37 @@
+import rpc from './lib/rpc.js'
+import NostrEmitter from '../../../index.js'
+import { getRandomBase64 } from './lib/crypto.js'
+
+const relayUrl = 'wss://nostr-relay.wlvs.space'
+const secret   = getRandomBase64()
+
+const connectString = Buffer.from(`${relayUrl}:${secret}`).toString('base64url')
+const emitter = new NostrEmitter()
+
+emitter.on('getinfo', async () => {
+  console.log('Received getinfo event.')
+  try {
+    const balance = await rpc('getbalance'),
+          blockct = await rpc('getblockcount')
+    const { chain } = await rpc('getblockchaininfo')
+    emitter.emit('nodeinfo', { balance, blockct, chain })
+  } catch(err) { errorHandler(err) }
+})
+
+emitter.on('call', async data => {
+  console.log('Received request:', data)
+  try {
+    const [ method, ...params ] = data
+    const result = await rpc(method, params)
+    emitter.emit('response', result)
+  } catch (err) { errorHandler(err) }
+})
+
+function errorHandler(err) {
+  console.error(err)
+  emitter.emit('error', err.toString())
+}
+
+await emitter.connect('wss://' + relayUrl, secret)
+
+console.log(`Paste this connection string into your web app:\n\n${connectString}\n`)
