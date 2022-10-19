5b997750b87fb00dcdcf2d7b841879150d37a619
cmd <cmd@yggdrasil.onion>
2022-10-18 21:41:00 -0500

    update

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const { schnorr } = isBrowser
   ? window.nobleSecp256k1
   : require('@noble/secp256k1')
 
-const WebSocket = isBrowser ? window.WebSocket : require('ws');
-const crypto = isBrowser ? window.crypto : require('crypto').webcrypto;
+const WebSocket = isBrowser ? window.WebSocket : require('ws')
+const crypto = isBrowser ? window.crypto : require('crypto').webcrypto
 
 // Specify our base64 helper functions.
 const b64encode = isBrowser
@@ -18,12 +18,12 @@ const b64decode = isBrowser
   : (str) => hexToBytes(Buffer.from(str, 'base64').toString('utf8'))
 
 // Specify our text encoders.
-const ec = new TextEncoder();
-const dc = new TextDecoder();
+const ec = new TextEncoder()
+const dc = new TextDecoder()
 
 // Helper functions for encoding / decoding JSON.
-const JSONencode = (data) => JSON.stringify(data);
-const JSONdecode = (data) => JSON.parse(data);
+const JSONencode = (data) => JSON.stringify(data)
+const JSONdecode = (data) => JSON.parse(data)
 
 // Default options to use.
 const DEFAULT_OPT = {
@@ -33,7 +33,7 @@ const DEFAULT_OPT = {
   selfPub: false,  // React to self-published events.
   verbose: false,  // Show verbose log output.
   since: Math.floor(Date.now() / 1000)
-};
+}
 
 class NostrEmitter {
   // Our main class object.
@@ -43,15 +43,15 @@ class NostrEmitter {
   static utils = {}
 
   constructor(opt = {}) {
-    this.connected = false;
-    this.subscribed = false;
+    this.connected = false
+    this.subscribed = false
 
     this.keys = {
       priv: null,
       pub: null,
       shared: null,
       digest: null,
-    };
+    }
 
     this.events = { all: new Set() }
     this.tags = []
@@ -67,7 +67,7 @@ class NostrEmitter {
     this.filter = {
       kinds: [this.opt.kind],
       ...opt.filter,
-    };
+    }
 
     if (this.opt.since) this.filter.since = this.opt.since
   }
@@ -122,56 +122,55 @@ class NostrEmitter {
     this.socket.on('message', (event) => this.messageHandler(event))
 
     // Generate a new pair of signing keys.
-    const keys = await getSignKeys(this.signSecret);
+    const keys = await getSignKeys(this.signSecret)
 
     this.keys = {
       priv: keys[0],  // Private key.
       pub: keys[1],   // Public key.
       shared: await getSharedKey(this.secret),
       label: await new hash(this.secret, 2).hex(),
-    };
+    }
     
     // Configure our event tags and filter.
-    this.tags.push(['s', this.keys.label]);
-    this.filter['#s'] = [this.keys.label];
+    this.tags.push(['s', this.keys.label])
+    this.filter['#s'] = [this.keys.label]
 
     // Return a promise that includes a timeout.
     return new Promise((res, rej) => {
-      let count = 0,
-        retries = 10;
+      let count = 0, retries = 10
       let interval = setInterval(() => {
         if (this.connected && this.subscribed) {
-          this.log('Connected and subscribed!');
-          res(clearInterval(interval));
+          this.log('Connected and subscribed!')
+          res(clearInterval(interval))
         } else if (count > retries) {
-          this.log('Failed to connect!');
-          rej(clearInterval(interval));
+          this.log('Failed to connect!')
+          rej(clearInterval(interval))
         } else {
-          count++;
+          count++
         }
-      }, 500);
-    });
+      }, 500)
+    })
   }
 
   decodeEvent(event) {
     // Decode an incoming event.
     return event instanceof Uint8Array
       ? JSONdecode(event.toString('utf8'))
-      : JSONdecode(event.data);
+      : JSONdecode(event.data)
   }
 
   async decryptContent(content) {
     // Decrypt content of a message.
     return decrypt(content, this.keys.shared)
       .then((data) => JSONdecode(data))
-      .catch((err) => console.error(err));
+      .catch((err) => console.error(err))
   }
 
   async openHandler(event) {
     /** Handle the socket open event. */
-    this.log('Socket connected to: ', this.relayUrl);
-    this.connected = true;
-    this.subscribe();
+    this.log('Socket connected to: ', this.relayUrl)
+    this.connected = true
+    this.subscribe()
   }
 
   messageHandler(event) {
@@ -197,7 +196,7 @@ class NostrEmitter {
   }
 
   async eventHandler(data) {
-    const { content, ...metaData } = data;
+    const { content, ...metaData } = data
 
     // If the event is from ourselves, 
     if (metaData?.pubkey === this.keys.pub) {
@@ -206,11 +205,19 @@ class NostrEmitter {
     }
 
     // Decrypt the message content.
-    const decryptedContent = await this.decryptContent(content);
+    const decryptedContent = await this.decryptContent(content)
    
     if (this.opt.verbose) {
       this.log('content: ' + JSON.stringify(decryptedContent, null, 2))
       this.log('metaData: ' + JSON.stringify(metaData, null, 2))
+    }
+
+    // If the decrypted content is empty, destroy the event.
+    if (!decryptedContent) {
+      return this.emit('destroy', null, {
+        kind: 5,
+        tags: [[ 's', metaData.id ]]
+      })
     }
 
     // Unpack the decrypted content.
@@ -232,7 +239,7 @@ class NostrEmitter {
 
   async send(eventName, eventData, eventMsg = { tags: [] }) {
     /** Send a data message to the relay. */
-    const serialData = JSONencode({ eventName, eventData });
+    const serialData = JSONencode({ eventName, eventData })
     
     const event = {
       content    : await encrypt(serialData, this.keys.shared),
@@ -247,10 +254,10 @@ class NostrEmitter {
     }
 
     // Sign our message.
-    const signedEvent = await this.getSignedEvent(event);
+    const signedEvent = await this.getSignedEvent(event)
 
     // Serialize and send our message.
-    this.socket.send(JSONencode(['EVENT', signedEvent]));
+    this.socket.send(JSONencode(['EVENT', signedEvent]))
   }
 
   async getSignedEvent(event) {
@@ -264,7 +271,7 @@ class NostrEmitter {
       event['kind'],
       event['tags'],
       event['content'],
-    ]);
+    ])
 
     // Append event ID and signature
     event.id = await new hash(eventData).hex()
@@ -280,7 +287,7 @@ class NostrEmitter {
       event.sig = bytesToHex(event.sig)
     }
 
-    return event;
+    return event
   }
 
   _getEventListByName(eventName) {
@@ -288,26 +295,26 @@ class NostrEmitter {
      *  else return the stored subscriber list.
      * */
     if (typeof this.events[eventName] === 'undefined') {
-      this.events[eventName] = new Set();
+      this.events[eventName] = new Set()
     }
     return this.events[eventName]
   }
 
   on(eventName, fn) {
     /** Subscribe function to run on a given event. */
-    this._getEventListByName(eventName).add(fn);
+    this._getEventListByName(eventName).add(fn)
   }
 
   once(eventName, fn) {
     /** Subscribe function to run once, using
      *  a callback to cancel the subscription.
      * */
-    const self = this;
+    const self = this
 
     const onceFn = function (...args) {
       self.remove(eventName, onceFn)
       fn.apply(self, args)
-    };
+    }
 
     this.on(eventName, onceFn)
   }
@@ -374,75 +381,75 @@ async function getSignKeys(secret) {
    */
   const privateKey = secret
     ? await new hash(secret).bytes()
-    : getRandomBytes(32);
+    : getRandomBytes(32)
   const publicKey = schnorr.getPublicKey(privateKey)
   return [
     bytesToHex(new Uint8Array(privateKey)),
     bytesToHex(new Uint8Array(publicKey)),
-  ];
+  ]
 }
 
 async function getSharedKey(string) {
   /** Derive a shared key-pair and import as a
    *  CryptoKey object (for Webcrypto library).
    */
-  const secret = await new hash(string).bytes(),
-    options = { name: 'AES-CBC' },
-    usage = ['encrypt', 'decrypt'];
-  return crypto.subtle.importKey('raw', secret, options, true, usage);
+  const secret  = await new hash(string).bytes()
+  const options = { name: 'AES-CBC' }
+  const usage   = ['encrypt', 'decrypt']
+  return crypto.subtle.importKey('raw', secret, options, true, usage)
 }
 
 async function encrypt(message, keyFile) {
   /** Encrypt a message using a CryptoKey object.
    * */
-  const iv = crypto.getRandomValues(new Uint8Array(16));
+  const iv = crypto.getRandomValues(new Uint8Array(16))
   const cipherBytes = await crypto.subtle
     .encrypt({ name: 'AES-CBC', iv }, keyFile, ec.encode(message))
-    .then((bytes) => new Uint8Array(bytes));
+    .then((bytes) => new Uint8Array(bytes))
   // Return a concatenated and base64 encoded array.
-  return b64encode(new Uint8Array([...iv, ...cipherBytes]));
+  return b64encode(new Uint8Array([...iv, ...cipherBytes]))
 }
 
 async function decrypt(encodedText, keyFile) {
   /** Decrypt an encrypted message using a CryptoKey object.
    * */
-  const bytes = b64decode(encodedText);
+  const bytes = b64decode(encodedText)
   const plainText = await crypto.subtle.decrypt(
     { name: 'AES-CBC', iv: bytes.slice(0, 16) },
     keyFile,
     bytes.slice(16)
-  );
-  return dc.decode(plainText);
+  )
+  return dc.decode(plainText)
 }
 
 function bytesToHex(byteArray) {
-  for (var arr = [], i = 0; i < byteArray.length; i++) {
-    arr.push(byteArray[i].toString(16).padStart(2, '0'));
+  for (let arr = [], i = 0; i < byteArray.length; i++) {
+    arr.push(byteArray[i].toString(16).padStart(2, '0'))
   }
-  return arr.join('');
+  return arr.join('')
 }
 
 function hexToBytes(str) {
-  for (var arr = [], i = 0; i < str.length; i += 2) {
-    arr.push(parseInt(str.substr(i, 2), 16));
+  for (let arr = [], i = 0; i < str.length; i += 2) {
+    arr.push(parseInt(str.substr(i, 2), 16))
   }
-  return Uint8Array.from(arr);
+  return Uint8Array.from(arr)
 }
 
 function sign(msg, key) {
-  return schnorr.sign(msg, key);
+  return schnorr.sign(msg, key)
 }
 
 function verify(sig, msgHash, pubKey) {
-  return schnorr.verify(sig, msgHash, pubKey);
+  return schnorr.verify(sig, msgHash, pubKey)
 }
 
 function getRandomBytes(size = 32) {
-  return crypto.getRandomValues(new Uint8Array(size));
+  return crypto.getRandomValues(new Uint8Array(size))
 }
 
 function getRandomString(size = 32) {
-  return bytesToHex(getRandomBytes(size));
+  return bytesToHex(getRandomBytes(size))
 }
 
 function encodeShareLink(secret, relayUrl) {
@@ -479,7 +486,7 @@ NostrEmitter.utils = {
 
 // Handle exports between browser and node.
 if (isBrowser) {
-  window.NostrEmitter = NostrEmitter;
+  window.NostrEmitter = NostrEmitter
 } else {
-  module.exports = NostrEmitter;
+  module.exports = NostrEmitter
 }
